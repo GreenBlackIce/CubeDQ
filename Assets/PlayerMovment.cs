39642325c95e08209bd9d69a3d61249341c5d8ad
Bernard, Xaver <xaver.bernard@web.de>
2021-06-01 00:53:50 +0200

    Added Player animation

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
 
-    public CharacterController controller;
+    public CharacterController controller; 
     public float speed = 12f;
-
     public float smoothingTime = 0.1f;
     private float smootingVel;
+
+    void Start() 
+    {
+        gameObject.GetComponent<Animator>().enabled = false;    
+    }
 
     void Update()
     {
@@ -20,10 +24,15 @@ public class PlayerMovment : MonoBehaviour
 
         if(direction.magnitude >= 0.01f)
         {
+            gameObject.GetComponent<Animator>().enabled = true;
             float targetAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref smootingVel, smoothingTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             controller.Move(direction * speed * Time.deltaTime);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
         }
     }
 }
